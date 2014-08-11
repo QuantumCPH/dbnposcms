d2d6bef6f1104c2692a72f193776d1bbed8d0138
khanmuhammad <khanmuhammadmalik@gmail.com>
2014-08-11 11:22:25 +0500

    dsfdsfds

--- web/server_processing.php
+++ web/server_processing.php
@@ -206,7 +206,7 @@ while ($aRow = mysql_fetch_array($rResult)) {
     }
     $output['aaData'][] = $row;
 
-    var_dump($output);
+//    var_dump($output);
 }
 echo json_encode(array_map('utf8_encode', $output));
  
