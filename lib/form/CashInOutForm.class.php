f1856ac812208e6ec7cf2af0a9b613b7f3f3e3dd
Globit <riaz_qadeer90@yahoo.com>
2015-02-05 17:04:18 +0500

    Error Fixed

---
+++ lib/form/CashInOutForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * CashInOut form.
+ *
+ * @package    zapnacrm
+ * @subpackage form
+ * @author     Your name here
+ */
+class CashInOutForm extends BaseCashInOutForm
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/model/CashInOut.php
@@ -0,0 +1,5 @@
+<?php
+
+class CashInOut extends BaseCashInOut
+{
+}
