79d1f4c0118173579b68829722f50c2c4ae6de2b
Globit <riaz_qadeer90@yahoo.com>
2015-02-05 15:33:11 +0500

    Error Fixed

---
+++ lib/form/ReturnReceiptsForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * ReturnReceipts form.
+ *
+ * @package    zapnacrm
+ * @subpackage form
+ * @author     Your name here
+ */
+class ReturnReceiptsForm extends BaseReturnReceiptsForm
+{
+  public function configure()
+  {
+  }
+}
