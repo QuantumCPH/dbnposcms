f0e8984c87bf02a028111cda642b907e27816eed
Globit <riaz_qadeer90@yahoo.com>
2015-02-13 17:54:49 +0500

    Error fixing 13/2/15

---
+++ lib/filter/BookoutFormFilter.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Bookout filter form.
+ *
+ * @package    zapnacrm
+ * @subpackage filter
+ * @author     Your name here
+ */
+class BookoutFormFilter extends BaseBookoutFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/base/BaseReceiptsForm.class.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Receipts form base class.
+ *
+ * @package    zapnacrm
+ * @subpackage form
+ * @author     Your name here
+ */
+class BaseReceiptsForm extends BaseFormPropel
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'id'         => new sfWidgetFormInputHidden(),
+      'shop_id'    => new sfWidgetFormInput(),
+      'created_at' => new sfWidgetFormDateTime(),
+      'updated_at' => new sfWidgetFormDateTime(),
+      'updated_by' => new sfWidgetFormInput(),
+    ));
+
+    $this->setValidators(array(
+      'id'         => new sfValidatorPropelChoice(array('model' => 'Receipts', 'column' => 'id', 'required' => false)),
+      'shop_id'    => new sfValidatorInteger(array('required' => false)),
+      'created_at' => new sfValidatorDateTime(),
+      'updated_at' => new sfValidatorDateTime(),
+      'updated_by' => new sfValidatorString(array('max_length' => 255, 'required' => false)),
+    ));
+
+    $this->widgetSchema->setNameFormat('receipts[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'Receipts';
+  }
+
+
+}
