df42ceb9c0753edb81b2f5b5f32d6eab9dca26cd
Globit <riaz_qadeer90@yahoo.com>
2015-03-02 11:07:14 +0500

    Error Fixed

--- lib/GcmSync.class.php
+++ lib/GcmSync.class.php
@@ -62,11 +62,6 @@ class GcmLib {
             'Content-Type: application/json'
         );
 // Open connection
-        
-        
-        
-             
-        
         $ch = curl_init();
 
 // Set the url, number of POST vars, POST data
