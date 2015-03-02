9ff1af01f5215992120ed5c06fc25f4391db1552
Globit <riaz_qadeer90@yahoo.com>
2015-03-02 18:08:53 +0500

    Error Fixed

--- apps/b2c/modules/pScripts/actions/actions.class.php
+++ apps/b2c/modules/pScripts/actions/actions.class.php
@@ -2019,7 +2019,7 @@ Have a great day!';
         //$orderjson=str_replace("?","0",$data_string);
         //$json_from_orders = json_decode($orderjson);
        var_dump($json_from_orders);
-die();
+
         $i = 0;
         $a = "";
         $orderIdArr = "";
